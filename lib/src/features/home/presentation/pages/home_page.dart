34adcb35363d3ae9ee29bebb04baee368861ca57
Raj <rs457036@gmail.com>
2022-02-25 22:45:06 +0530

    _

--- lib/src/features/home/presentation/pages/home_page.dart
+++ lib/src/features/home/presentation/pages/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/configs/app_configs.dart';
-import '../../../../core/constants/constraints/constraints.dart';
 import '../../../shared/auth/presentation/blocs/auth_bloc/auth_bloc.dart';
 
 /// {@template home_page}
